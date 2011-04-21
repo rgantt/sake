ba5cf18c0fdedaf74ffbefdbc9b13b3135e4f680
gavinchi <ryan@zuttonet.com>
2011-04-20 23:10:38 -0600

    proper indentation

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ require_once SAKE_ROOT.'/config/config.php';
 require_once SAKE_ROOT.'/vendor/sake/sake.php';
 
 try
-{   
+{
 	/**
 	 * bootstrap the dispatcher and then process the request
 	 */
-    list( $controller, $request, $response, $output ) = biru_controller\dispatcher::dispatch( new biru_controller\cgi() );
-    echo $controller->process( $request, $response )->out( $output );
+	list( $controller, $request, $response, $output ) = biru_controller\dispatcher::dispatch( new biru_controller\cgi() );
+	echo $controller->process( $request, $response )->out( $output );
 }
 catch( \sake_exception $e )
 {
-    echo $e->unwind();
+	echo $e->unwind();
 }
